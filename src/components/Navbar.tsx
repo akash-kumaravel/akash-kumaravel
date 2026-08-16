13cc10977d6c633d7a6f218b1ef4565d498ea854
Akash <akashuxui@gmail.com>
2026-08-16 18:36:44 +0530

    Add files via upload

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -109,22 +109,20 @@ export default function Navbar({ onOpenModal, activePage = 'home', onNavigateHom
             </svg>
           </a>
 
-          {/* X / Twitter Link */}
+          {/* Behance Link */}
           <a
-            href={FOOTER_DATA.twitter}
+            href={FOOTER_DATA.behance}
             target="_blank"
             rel="noopener noreferrer"
             className="flex items-center justify-center w-10 h-10 bg-gray-50 hover:bg-gray-100 border border-gray-200/60 rounded-full transition-all duration-200 hover:scale-105"
-            title="X (Twitter)"
-            aria-label="X (Twitter)"
+            title="Behance"
+            aria-label="Behance"
           >
-            <svg width="20" height="20" viewBox="0 0 24 24" fill="none">
-              <path
-                fillRule="evenodd"
-                clipRule="evenodd"
-                d="M12 24c6.627 0 12-5.373 12-12S18.627 0 12 0 0 5.373 0 12s5.373 12 12 12ZM9.95 12.6 5 18h2l3.818-4.242L14 18h4l-.75-1-4.182-5.575L18 6h-2l-3.797 4.271L9 6H5l.75 1 4.2 5.6Zm1.55.4L7 7h1.5L16 17h-1.5l-3-4Z"
-                fill="#202020"
-              />
+            <svg width="20" height="20" viewBox="0 0 512 512" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M344.1,233.6c-28.9,0-32.9,28.8-32.9,28.8h61.4S373,233.6,344.1,233.6Z" fill="#202020"/>
+              <path d="M204.8,262.4H150.4v50h51.7c7.8-.2,22.4-2.4,22.4-24.3C224.5,262.1,204.8,262.4,204.8,262.4Z" fill="#202020"/>
+              <path d="M256,32C132.3,32,32,132.3,32,256S132.3,480,256,480,480,379.7,480,256,379.7,32,256,32Zm47.2,137.6h77.1v23H303.2v-23Zm-39,120.8c0,57-59.4,55.2-59.4,55.2H107.6v-187h97.2c29.6,0,52.9,16.3,52.9,49.8S229.2,244,229.2,244C266.8,244,264.2,290.4,264.2,290.4Zm144.2-3.1H311.5c0,34.7,32.9,32.5,32.9,32.5c31.1,0,30-20.1,30-20.1h32.9c0,53.4-64,49.7-64,49.7c-76.7,0-71.8-71.5-71.8-71.5s-.1-71.8,71.8-71.8C419,206.2,408.4,287.3,408.4,287.3Z" fill="#202020"/>
+              <path d="M218,211.3c0-19.4-13.2-19.4-13.2-19.4H150.4v41.7h51C210.2,233.6,218,230.7,218,211.3Z" fill="#202020"/>
             </svg>
           </a>
         </div>
--- src/data/portfolioData.ts
+++ src/data/portfolioData.ts
@@ -6,9 +6,10 @@ export const FOOTER_DATA = {
   name: "Akash Kumaravel",
   role: "Graphic & UI/UX Designer",
   bio: "Crafting digital experiences, apps, design systems, and creative tools.",
-  linkedin: "https://www.linkedin.com/in/akashkumaravel/",
+  linkedin: "https://www.linkedin.com/in/akash-kumaravel/",
   twitter: "https://x.com/akashkumaravel",
   github: "https://github.com/akashkumaravel",
+  behance: "https://www.behance.net/akashkumaravel",
   email: "akashuxui@gmail.com",
   resume: RESUME_PDF_URL,
 };
